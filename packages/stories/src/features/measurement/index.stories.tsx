17c068ef11f3404e91d36f4a0afc55a91e2bfd8c
Riyazahmed Panarwala <35068273+riyazpanarwala@users.noreply.github.com>
2024-12-03 15:31:34 +0530

    Add files via upload

---
+++ packages/stories/src/features/measurement/index.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import Measurement from "./measurement";
+
+export default {
+    component: Measurement,
+    title: "Features/measurement",
+};
+
+export const MeasurementTool = () => <Measurement />;

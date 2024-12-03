7e20c2bb59f1d6f8c84aa546acf4c7c4b3893f6c
Riyazahmed Panarwala <35068273+riyazpanarwala@users.noreply.github.com>
2024-12-03 15:23:52 +0530

    Add files via upload

--- packages/interactive/src/index.ts
+++ packages/interactive/src/index.ts
@@ -10,4 +10,5 @@ export { InteractiveYCoordinate } from "./InteractiveYCoordinate";
 export { DrawingObjectSelector } from "./DrawingObjectSelector";
 export { ZoomButtons } from "./ZoomButtons";
 export { Measurement } from "./Measurement";
+export { RangeProfileSeries } from "./RangeProfileSeries";
 export * from "./utils";

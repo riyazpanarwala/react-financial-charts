fe084dd5ba6b39022eafd3dfb43d9aa25574dd8a
Riyazahmed Panarwala <35068273+riyazpanarwala@users.noreply.github.com>
2024-12-05 12:45:21 +0530

    Create index.stories.tsx

---
+++ packages/stories/src/features/Brush/index.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import Brush from "./brush";
+
+export default {
+    component: Brush,
+    title: "Features/Brush",
+};
+
+export const BrushComp = () => <Brush />;

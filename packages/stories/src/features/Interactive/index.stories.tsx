32f6f1e1fbb674f3aa7aff76f3c2b1ebae730866
Riyazahmed Panarwala <35068273+riyazpanarwala@users.noreply.github.com>
2024-12-05 12:49:44 +0530

    Create index.stories.tsx

---
+++ packages/stories/src/features/Interactive/index.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import InteractiveY from "./interactiveY";
+
+export default {
+    component: InteractiveY,
+    title: "Features/interactiveY",
+};
+
+export const InteractiveYCoordinates = () => <InteractiveY />;

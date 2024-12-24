00796b56462eaafd3f818091fda09f23dfcbcaaf
Riyazahmed Panarwala <35068273+riyazpanarwala@users.noreply.github.com>
2024-12-24 15:35:21 +0530

    eslint 9 update

---
+++ eslint.config.mjs
@@ -0,0 +1,76 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+export default [
+    {
+        ignores: ["**/node_modules", "**/lib", "**/coverage", "eslint.config.mjs"],
+    },
+    ...compat.extends(
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+    ),
+    {
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+
+            parser: tsParser,
+            ecmaVersion: 5,
+            sourceType: "commonjs",
+
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+
+        rules: {
+            "@typescript-eslint/explicit-member-accessibility": "off",
+            "@typescript-eslint/explicit-module-boundary-types": "off",
+            "@typescript-eslint/interface-name-prefix": "off",
+            "@typescript-eslint/no-non-null-assertion": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
+            "@typescript-eslint/ban-types": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/no-unsafe-function-type": "off",
+            "@typescript-eslint/no-empty-object-type": "off",
+            "react/display-name": "off",
+            "react/prop-types": "off",
+            "arrow-spacing": "error",
+            curly: "error",
+            "no-duplicate-imports": "error",
+            "no-multiple-empty-lines": "error",
+            "no-var": "error",
+            "prefer-rest-params": 0,
+        },
+    },
+];
